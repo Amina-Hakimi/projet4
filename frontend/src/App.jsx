723af0592540833dc778287cae96204b499467e7
Amina HAKIMI <amina.hakimi86@gmail.com>
2023-02-03 16:38:55 +0100

    Création de la page Un atelier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "@components/Navbar";
 import "./App.css";
 import Ateliers from "@pages/Ateliers";
-
+import OneAtelier from "@pages/OneAtelier";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-       <Routes>
-        <Route path="/ateliers" element={<Ateliers />} />
-       </Routes>
-        
+        <Routes>
+          <Route path="/ateliers" element={<Ateliers />} />
+          <Route path="/ateliers/:id" element={<OneAtelier />} />
+        </Routes>
       </div>
     </Router>
   );
--- frontend/src/components/CardAtelier.jsx
+++ frontend/src/components/CardAtelier.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CardAtelier({ atel }) {
   return (
-    <div className="text-black">
-      <div className="shadow-md shadow-gray-600 rounded-lg">
-        <h3 className="text-lg font-semibold text-center my-3">{atel.name}</h3>
-        <img
-          className="rounded-md duration-200 hover:scale-105"
-          src={`${import.meta.env.VITE_BACKEND_URL}/${atel.image}`}
-          alt={`${import.meta.env.VITE_BACKEND_URL}/${atel.name}`}
-        />
+    <Link to={`/ateliers/${atel.idateliers}`}>
+      <div className="text-black">
+        <div className="shadow-md shadow-gray-600 rounded-lg">
+          <h3 className="text-lg font-semibold text-center my-3">
+            {atel.name}
+          </h3>
+          <img
+            className="rounded-md duration-200 hover:scale-105"
+            src={`${import.meta.env.VITE_BACKEND_URL}/${atel.image}`}
+            alt={`${import.meta.env.VITE_BACKEND_URL}/${atel.name}`}
+          />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
